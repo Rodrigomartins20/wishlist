fc4339ac16a91914eff17b5ee48bd1464ca64884
leonardovee <leoveeph@gmail.com>
2022-01-31 23:28:54 -0300

    test: ensure ClientController returns FindClientInterface values

--- __tests__/client-controller.spec.ts
+++ __tests__/client-controller.spec.ts
@@ -4,7 +4,11 @@ import ClientController from '../src/presentation/controllers/client-controller'
 const mockFindClientInterface = (): FindClientInterface => {
   class FindClient implements FindClientInterface {
     find (id: string): any {
-      return null
+      return {
+        id: 'e90b6e65-d87f-4fe3-b074-9ad1599bc9c7',
+        name: 'any name',
+        email: 'any@mail.com'
+      }
     }
   }
   return new FindClient()
@@ -31,6 +35,15 @@ describe('Client Controller', () => {
       const findClientSpy = jest.spyOn(findClientStub, 'find')
       await sut.find('e90b6e65-d87f-4fe3-b074-9ad1599bc9c7')
       expect(findClientSpy).toHaveBeenCalledWith('e90b6e65-d87f-4fe3-b074-9ad1599bc9c7')
+    })
+    it('should return FindClient values', async () => {
+      const { sut } = makeSut()
+      const response = await sut.find('e90b6e65-d87f-4fe3-b074-9ad1599bc9c7')
+      expect(response).toEqual({
+        id: 'e90b6e65-d87f-4fe3-b074-9ad1599bc9c7',
+        name: 'any name',
+        email: 'any@mail.com'
+      })
     })
   })
 })
