dd0503ac7866be1a26401d6f944bc4ecd8767fd1
leonardovee <leoveeph@gmail.com>
2022-02-01 08:17:58 -0300

    test: ensure DbAllClientUsecase returns PostClientRepository values

--- __tests__/data/usecases/db-all-client-usecase.spec.ts
+++ __tests__/data/usecases/db-all-client-usecase.spec.ts
@@ -2,31 +2,37 @@ import { DbAllClientUsecase } from '../../../src/data/usecases/db-all-client-use
 import { AllClientRepositoryInterface } from '../../../src/data/protocols/db/all-client-repository-interface'
 import { FindClientRepositoryInterface } from '../../../src/data/protocols/db/find-client-repository-interface'
 import { DeleteClientRepositoryInterface } from '../../../src/data/protocols/db/delete-client-repository-interface'
+import { PostClientRepositoryInterface } from '../../../src/data/protocols/db/post-client-repository-interface'
 import { mockAllClientRepository } from '../../../__mocks__/all-client-repository-mock'
 import { mockFindClientRepository } from '../../../__mocks__/find-client-repository-mock'
 import { mockDeleteClientRepository } from '../../../__mocks__/delete-client-repository-mock'
+import { mockPostClientRepository } from '../../../__mocks__/post-client-repository-mock'
 
 type SutTypes = {
   sut: DbAllClientUsecase,
   allClientRepositoryStub: AllClientRepositoryInterface
   findClientRepositoryStub: FindClientRepositoryInterface
   deleteClientRepositoryStub: DeleteClientRepositoryInterface
+  postClientRepositoryStub: PostClientRepositoryInterface
 }
 
 const makeSut = (): SutTypes => {
+  const postClientRepositoryStub = mockPostClientRepository()
   const deleteClientRepositoryStub = mockDeleteClientRepository()
   const findClientRepositoryStub = mockFindClientRepository()
   const allClientRepositoryStub = mockAllClientRepository()
   const sut = new DbAllClientUsecase(
     allClientRepositoryStub,
     findClientRepositoryStub,
-    deleteClientRepositoryStub
+    deleteClientRepositoryStub,
+    postClientRepositoryStub
   )
   return {
     sut,
     allClientRepositoryStub,
     findClientRepositoryStub,
-    deleteClientRepositoryStub
+    deleteClientRepositoryStub,
+    postClientRepositoryStub
   }
 }
 
@@ -112,6 +118,50 @@ describe('Db All Client Usecase', () => {
       const promise = sut.delete('e90b6e65-d87f-4fe3-b074-9ad1599bc9c7')
 
       await expect(promise).rejects.toThrow()
+    })
+  })
+
+  describe('Post', () => {
+    it('should call ClientRepository', async () => {
+      const { sut, postClientRepositoryStub } = makeSut()
+      const postClientRepositorySpy = jest.spyOn(postClientRepositoryStub, 'post')
+
+      await sut.post({
+        id: 'e90b6e65-d87f-4fe3-b074-9ad1599bc9c7',
+        name: 'any name',
+        email: 'any@mail.com'
+      })
+
+      expect(postClientRepositorySpy).toHaveBeenCalledTimes(1)
+    })
+
+    it('should throw if ClientRepository throws', async () => {
+      const { sut, postClientRepositoryStub } = makeSut()
+      jest.spyOn(postClientRepositoryStub, 'post').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error('any message'))))
+
+      const promise = sut.post({
+        id: 'e90b6e65-d87f-4fe3-b074-9ad1599bc9c7',
+        name: 'any name',
+        email: 'any@mail.com'
+      })
+
+      await expect(promise).rejects.toThrow()
+    })
+
+    it('should return ClientRepository values', async () => {
+      const { sut } = makeSut()
+
+      const response = await sut.post({
+        id: 'e90b6e65-d87f-4fe3-b074-9ad1599bc9c7',
+        name: 'any name',
+        email: 'any@mail.com'
+      })
+
+      expect(response).toEqual({
+        id: 'e90b6e65-d87f-4fe3-b074-9ad1599bc9c7',
+        name: 'any name',
+        email: 'any@mail.com'
+      })
     })
   })
 })
