3577511bb51e8ac493f0dd038f937bed47c02593
leonardovee <leoveeph@gmail.com>
2022-01-31 23:34:39 -0300

    feat: ensure ClientController returns and error message FindClientInterface throws

--- src/domain/usecases/find-client-interface.ts
+++ src/domain/usecases/find-client-interface.ts
@@ -1,5 +1,5 @@
 import { ClientModel } from '../models/client-model'
 
 export default interface FindClientInterface {
-  find (id: string): ClientModel;
+  find (id: string): Promise<ClientModel>;
 }
--- src/presentation/controllers/client-controller.ts
+++ src/presentation/controllers/client-controller.ts
@@ -4,6 +4,10 @@ export default class ClientController {
   constructor (private readonly findClient: FindClientInterface) {}
 
   async find(id: string) {
-    return await this.findClient.find(id)
+    try {
+      return await this.findClient.find(id)
+    } catch (error) {
+      return { message: 'oops', error: error.message }
+    }
   }
 }
