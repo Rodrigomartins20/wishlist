98b2d4b5e6ffb830ad95b69680044284bfa8a3d8
leonardovee <leoveeph@gmail.com>
2022-02-01 07:57:50 -0300

    test: ensure DbAllClientUsecase returns AllClientRepository values

--- __tests__/data/usecases/db-all-client-usecase.spec.ts
+++ __tests__/data/usecases/db-all-client-usecase.spec.ts
@@ -35,5 +35,17 @@ describe('Db All Client Usecase', () => {
 
       await expect(promise).rejects.toThrow()
     })
+
+    it('should return ClientRepository values', async () => {
+      const { sut } = makeSut()
+
+      const response = await sut.all()
+
+      expect(response).toEqual([{
+        id: 'e90b6e65-d87f-4fe3-b074-9ad1599bc9c7',
+        name: 'any name',
+        email: 'any@mail.com'
+      }])
+    })
   })
 })
