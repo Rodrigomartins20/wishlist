1143ffd417f1213ab97f3328511ce041ba4e2cbb
leonardovee <leoveeph@gmail.com>
2022-01-31 23:07:42 -0300

    chore: config jest

---
+++ __tests__/client-controller.spec.ts
@@ -0,0 +1,7 @@
+describe('Client Controller', () => {
+  describe('Find', () => {
+    it('should call Client Service with correct values', async () => {
+      expect(true).toBe(true)
+    })
+  })
+})
