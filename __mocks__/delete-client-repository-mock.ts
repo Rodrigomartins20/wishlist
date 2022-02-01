a6daa0e064c219a7f7868347cfe7c2ef8dfb79ee
leonardovee <leoveeph@gmail.com>
2022-02-01 08:12:11 -0300

    feat: ensure DbAllClientUsecase returns DeleteClientRepository values

---
+++ __mocks__/delete-client-repository-mock.ts
@@ -0,0 +1,11 @@
+import { DeleteClientRepositoryInterface } from '../src/data/protocols/db/delete-client-repository-interface'
+import DeleteClientInterface from '../src/domain/usecases/delete-client-interface'
+
+export const mockDeleteClientRepository = (): DeleteClientInterface => {
+  class DeleteClientRepository implements DeleteClientRepositoryInterface {
+    async delete (id: string): Promise<void> {
+      return new Promise(resolve => resolve())
+    }
+  }
+  return new DeleteClientRepository()
+}
--- src/data/usecases/db-all-client-usecase.ts
+++ src/data/usecases/db-all-client-usecase.ts
@@ -3,12 +3,18 @@ import AllClientInterface from '@/domain/usecases/all-client-interface'
 import { AllClientRepositoryInterface } from '@/data/protocols/db/all-client-repository-interface'
 import FindClientInterface from '@/domain/usecases/find-client-interface'
 import { FindClientRepositoryInterface } from '../protocols/db/find-client-repository-interface'
+import { DeleteClientRepositoryInterface } from '../protocols/db/delete-client-repository-interface'
+import DeleteClientInterface from '@/domain/usecases/delete-client-interface'
 
-export class DbAllClientUsecase implements AllClientInterface, FindClientInterface {
+export class DbAllClientUsecase implements AllClientInterface, FindClientInterface, DeleteClientInterface {
   constructor (
     private readonly allClientRepository: AllClientRepositoryInterface,
-    private readonly findClientRepository: FindClientRepositoryInterface
+    private readonly findClientRepository: FindClientRepositoryInterface,
+    private readonly deleteClientRepository: DeleteClientRepositoryInterface
   ) {}
+  async delete(id: string): Promise<void> {
+    await this.deleteClientRepository.delete(id)
+  }
   async find(id: string): Promise<ClientModel> {
     return await this.findClientRepository.find(id)
   }
