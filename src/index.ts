13ee0373e673a998eb2d0fdba89b3f9b602b8916
leonardovee <leoveeph@gmail.com>
2022-02-01 13:26:20 -0300

    feat: script to run migrations

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log('bora')
+import dotenv from 'dotenv'
+dotenv.config()
+
+import fs from 'fs'
+import { MysqlHelper } from './infra/db/mysql-helper'
+
+const run = async () => {
+  const mysql = new MysqlHelper()
+  await mysql.connect()
+
+  console.log('Running migrations.')
+  fs.readFileSync('./migrations/schema.sql', 'utf-8').toString().split(';')
+    .forEach(async query => {
+      if (!query) return
+      await mysql.query(query)
+    })
+  console.log('Done!')
+}
+
+run()
