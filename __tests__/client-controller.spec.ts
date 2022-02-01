488fd85aeffeb9538198a090be4238200f1ae109
leonardovee <leoveeph@gmail.com>
2022-01-31 23:26:53 -0300

    test: ensure ClientController calls FindClientInterface with correct values

--- __tests__/client-controller.spec.ts
+++ __tests__/client-controller.spec.ts
@@ -1,7 +1,36 @@
+import FindClientInterface from '../src/domain/usecases/find-client-interface'
+import ClientController from '../src/presentation/controllers/client-controller'
+
+const mockFindClientInterface = (): FindClientInterface => {
+  class FindClient implements FindClientInterface {
+    find (id: string): any {
+      return null
+    }
+  }
+  return new FindClient()
+}
+
+type SutTypes = {
+  sut: ClientController,
+  findClientStub: FindClientInterface
+}
+
+const makeSut = (): SutTypes => {
+  const findClientStub = mockFindClientInterface()
+  const sut = new ClientController(findClientStub)
+  return {
+    sut,
+    findClientStub
+  }
+}
+
 describe('Client Controller', () => {
   describe('Find', () => {
-    it('should call Client Service with correct values', async () => {
-      expect(true).toBe(true)
+    it('should call FindClient with correct values', async () => {
+      const { sut, findClientStub } = makeSut()
+      const findClientSpy = jest.spyOn(findClientStub, 'find')
+      await sut.find('e90b6e65-d87f-4fe3-b074-9ad1599bc9c7')
+      expect(findClientSpy).toHaveBeenCalledWith('e90b6e65-d87f-4fe3-b074-9ad1599bc9c7')
     })
   })
 })
