fe97c9036615495901b6aa9f1a5cf4848373c5e4
leonardovee <leoveeph@gmail.com>
2022-02-03 23:17:58 -0300

    feat: ensure DbPostClientWishlistUsecase calls FindClientRepository with correct values

--- src/data/usecases/db-post-client-wishlist-usecase.ts
+++ src/data/usecases/db-post-client-wishlist-usecase.ts
@@ -3,14 +3,17 @@ import PostClientWishlistRepositoryInterface from '@/data/interfaces/post-client
 import PostClientWishlistInterface from '@/domain/interfaces/post-client-wishlist-interface'
 import FindProductByIdRepositoryInterface from '../interfaces/find-product-by-id-repository-interface'
 import FindWishlistByProductIdRepositoryInterface from '../interfaces/find-wishlist-by-product-id-repository-interface'
+import FindClientRepositoryInterface from '../interfaces/find-client-repository-interface'
 
 export default class DbPostClientWishlistUsecase implements PostClientWishlistInterface {
   constructor (
     private readonly postClientWishlistRepository: PostClientWishlistRepositoryInterface,
     private readonly findProductByIdRepository: FindProductByIdRepositoryInterface, 
-    private readonly findWishlistByProductIdRepository: FindWishlistByProductIdRepositoryInterface
+    private readonly findWishlistByProductIdRepository: FindWishlistByProductIdRepositoryInterface,
+    private readonly findClientRepositoryInterface: FindClientRepositoryInterface
   ) {}
   async post(clientWishlist: ClientWishlistModel): Promise<ClientWishlistModel> {
+    await this.findClientRepositoryInterface.find(clientWishlist.client)
     const productExists = await this.findProductByIdRepository.findProductById(clientWishlist.product)
     if (! productExists) {
       throw new Error('product doesnt exists')
